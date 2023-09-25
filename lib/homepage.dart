06afafca24a00573d2c9c4fc09bd12377bc24e93
FASLA V M <141605898+FASLASHALU@users.noreply.github.com>
2023-09-25 12:03:13 +0530

    first

---
+++ lib/homepage.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class page1 extends StatefulWidget {
+  const page1({super.key});
+
+  @override
+  State<page1> createState() => _page1State();
+}
+
+class _page1State extends State<page1> {
+  List<String>items =<String>[
+    "malaylam",
+    "English",
+    "Hindhi",
+    "jhjkjj",
+    "jhjhuh",
+  ];
+  String dropdownvalue="Malayalam";
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            DropdownButton(
+                value: dropdownvalue,
+                icon: const Icon(Icons.keyboard_arrow_down),
+                items: items.map((String items) {
+                  return DropdownMenuItem(
+                    value: items,
+                    child: Text(items),
+                  );
+                }).toList(),
+                onChanged: (String? newValue) {
+                  setState(() {
+                    dropdownvalue = newValue!;
+                  });
+                })
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:dropdown/homepage.dart';
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(MaterialApp(home: page1(),debugShowCheckedModeBanner: false,));
+}
